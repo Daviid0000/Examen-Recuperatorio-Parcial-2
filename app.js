3febe6d9103d80b783f3c8c08c35b5323d1ed734
David Liva <davidliva01@gmail.com>
2023-07-05 15:31:13 -0300

    Se configuró el archivo .env y se agregaron lineas faltantes al archivo app.js para poder usarlo junto con el archivo .env

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
 const path = require('path');
 
+require('dotenv').config();
+
+const PORT = process.env.PORT || 6000;
 const app = express();
 
 // Middlewares
@@ -14,7 +16,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
 
-// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+
+// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    res.write(`<div>
+                <h1>
+                    404 - Ruta no encontrada</h1>
+                </h1>`)
+});
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(PORT, () => console.log('Server on port', PORT));
